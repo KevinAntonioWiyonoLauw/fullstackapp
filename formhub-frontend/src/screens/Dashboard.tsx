ac65fca5fb03b2e3b634c3253f7a1046ffb9fcb9
yanliu1111 <liuyan0504@gmail.com>
2023-05-20 16:58:17 -0600

    feat: dataGrid UI done

--- formhub-frontend/src/screens/Dashboard.tsx
+++ formhub-frontend/src/screens/Dashboard.tsx
@@ -1,6 +1,13 @@
 import { gql, useQuery } from "@apollo/client";
+import Styled from "@emotion/styled";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React from "react";
+
+const Container = Styled.div`
+  display: flex;
+  height: 100vw;
+  width: 100vw;
+`;
 
 const Dashboard: React.FC = () => {
   const { data, loading } = useQuery(
@@ -16,26 +23,28 @@ const Dashboard: React.FC = () => {
   );
   if (loading) return <div>Loading...</div>;
   const columns: GridColDef[] = [
-    { field: "id", headerName: "ID" },
-    { field: "submittedAt", headerName: "Submitted At" },
+    { field: "id", headerName: "ID", width: 200 },
+    { field: "submittedAt", headerName: "Submitted At", width: 200 },
   ];
   return (
-    <DataGrid
-      rows={data.submissions.map((submission: any) => ({
-        id: submission.id,
-        submittedAt: submission.submittedAt,
-      }))}
-      columns={columns}
-      // initialState={{
-      //   pagination: {
-      //     paginationModel: {
-      //       pageSize: 5,
-      //     },
-      //   },
-      // }}
-      // pageSizeOptions={[5]}
-      disableRowSelectionOnClick
-    />
+    <Container>
+      <DataGrid
+        rows={data.submissions.map((submission: any) => ({
+          id: submission.id,
+          submittedAt: submission.submittedAt,
+        }))}
+        columns={columns}
+        // initialState={{
+        //   pagination: {
+        //     paginationModel: {
+        //       pageSize: 5,
+        //     },
+        //   },
+        // }}
+        // pageSizeOptions={[5]}
+        disableRowSelectionOnClick
+      />
+    </Container>
   );
 };
 export default Dashboard;
