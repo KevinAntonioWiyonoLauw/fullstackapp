9a42c6e02fa2d312df5853ee24cb91852a53b6b4
Turbobot <turbobot@vercel.com>
2023-05-27 22:17:15 -0600

    feat(create-turbo): apply official-starter transform

--- formhub-frontend/src/gql/graphql.ts
+++ formhub-frontend/src/gql/graphql.ts
@@ -1,10 +1,16 @@
 /* eslint-disable */
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -19,21 +25,55 @@ export type Scalars = {
 };
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   submissions: Array<Submission>;
 };
 
 export type Submission = {
-  __typename?: 'Submission';
-  data: Scalars['JSON'];
-  id: Scalars['ID'];
-  submittedAt: Scalars['DateTime'];
+  __typename?: "Submission";
+  data: Scalars["JSON"];
+  id: Scalars["ID"];
+  submittedAt: Scalars["DateTime"];
 };
 
-export type SubmissionsQueryVariables = Exact<{ [key: string]: never; }>;
+// Scalars means that the type is a primitive type
 
+export type SubmissionsQueryVariables = Exact<{ [key: string]: never }>;
 
-export type SubmissionsQuery = { __typename?: 'Query', submissions: Array<{ __typename?: 'Submission', id: string, submittedAt: any, data: any }> };
+export type SubmissionsQuery = {
+  __typename?: "Query";
+  submissions: Array<{
+    __typename?: "Submission";
+    id: string;
+    submittedAt: any;
+    data: any;
+  }>;
+};
 
-
-export const SubmissionsDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Submissions"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"submissions"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"submittedAt"}},{"kind":"Field","name":{"kind":"Name","value":"data"}}]}}]}}]} as unknown as DocumentNode<SubmissionsQuery, SubmissionsQueryVariables>;
+export const SubmissionsDocument = {
+  kind: "Document",
+  definitions: [
+    {
+      kind: "OperationDefinition",
+      operation: "query",
+      name: { kind: "Name", value: "Submissions" },
+      selectionSet: {
+        kind: "SelectionSet",
+        selections: [
+          {
+            kind: "Field",
+            name: { kind: "Name", value: "submissions" },
+            selectionSet: {
+              kind: "SelectionSet",
+              selections: [
+                { kind: "Field", name: { kind: "Name", value: "id" } },
+                { kind: "Field", name: { kind: "Name", value: "submittedAt" } },
+                { kind: "Field", name: { kind: "Name", value: "data" } },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<SubmissionsQuery, SubmissionsQueryVariables>;
