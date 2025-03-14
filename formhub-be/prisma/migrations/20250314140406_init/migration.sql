eaa342c38a7a8833fd82682a38e29dcb86f30405
Kevin <kevinantoniowiyonolauw@mail.ugm.ac.id>
2025-03-14 21:20:28 +0700

    new commit

---
+++ formhub-be/prisma/migrations/20250314140406_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "submissions" (
+    "id" TEXT NOT NULL,
+    "submittedAt" TIMESTAMP(3) NOT NULL,
+    "data" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "submissions_pkey" PRIMARY KEY ("id")
+);
